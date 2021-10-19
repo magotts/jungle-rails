e5c8b2aee991aeed25e9c3b1865666b9b7741b60
magotts <techybet@gmail.com>
2021-10-19 13:11:55 -0400

    add downcase strip for email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email].downcase.strip)
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 5 }
+  before_save { self.email.downcase! }
 
   def self.authenticate_with_credentials(email, password)
     email = email.downcase.strip # converts to lower case and strips off the spaces in between
